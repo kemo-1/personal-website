57fbae6445912731b31511bbeee3bdb4fa1040d8
kemo-1 <abdullhakim.sami@gmail.com>
2025-05-13 03:10:20 +0300

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    server: {
+      allowedHosts:["wpm2df91hnqe.share.zrok.io"]
+
+    },
     plugins: [tailwindcss(
    
     )]
